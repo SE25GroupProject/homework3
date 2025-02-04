6ff5536aa9ec255e5256fcb3c00958be15b4fd26
Jacob Allard <allardj02@gmail.com>
2025-02-04 14:06:46 -0500

    Finished Testing and Analyzing

--- src/hw2_debugging.py
+++ src/hw2_debugging.py
@@ -1,47 +1,49 @@
-import src.rand as rand
+"""Homework 2 file for debugging and cleaning up"""
 
-def mergeSort(arr):
-    if (len(arr) == 1):
-        return arr
+from rand import random_array
 
-    half = len(arr)//2
+def merge_sort(array):
+    """Used to merge sort the array"""
+    if len(array) == 1:
+        return array
 
-    return recombine(mergeSort(arr[:half]), mergeSort(arr[half:]))
+    half = len(array)//2
 
-def recombine(leftArr, rightArr):
-    leftIndex = 0
-    rightIndex = 0
-    mergeArr = [None] * (len(leftArr) + len(rightArr))
-    while leftIndex < len(leftArr) and rightIndex < len(rightArr):
-        if leftArr[leftIndex] < rightArr[rightIndex]:
-            mergeArr[leftIndex + rightIndex] = leftArr[leftIndex]
-            leftIndex += 1
+    return recombine(merge_sort(array[:half]), merge_sort(array[half:]))
+
+def recombine(left_arr, right_arr):
+    """Used to recombine the array"""
+    left_index = 0
+    right_index = 0
+    merge_arr = [None] * (len(left_arr) + len(right_arr))
+    while left_index < len(left_arr) and right_index < len(right_arr):
+        if left_arr[left_index] < right_arr[right_index]:
+            merge_arr[left_index + right_index] = left_arr[left_index]
+            left_index += 1
         else:
-            mergeArr[leftIndex + rightIndex] = rightArr[rightIndex]
-            rightIndex += 1
+            merge_arr[left_index + right_index] = right_arr[right_index]
+            right_index += 1
 
-    while(rightIndex < len(rightArr)):
-        mergeArr[leftIndex + rightIndex] = rightArr[rightIndex]
-        rightIndex += 1
+    while right_index < len(right_arr):
+        merge_arr[left_index + right_index] = right_arr[right_index]
+        right_index += 1
 
-    while(leftIndex < len(leftArr)):
-        mergeArr[leftIndex + rightIndex] = leftArr[leftIndex]
-        leftIndex += 1
+    while left_index < len(left_arr):
+        merge_arr[left_index + right_index] = left_arr[left_index]
+        left_index += 1
 
-    for i in range(rightIndex, len(rightArr)):
-        mergeArr[leftIndex + rightIndex] = rightArr[i]
-    
-    for i in range(leftIndex, len(leftArr)):
-        mergeArr[leftIndex + rightIndex] = leftArr[i]
+    for i in range(right_index, len(right_arr)):
+        merge_arr[left_index + right_index] = right_arr[i]
 
-    return mergeArr
+    for i in range(left_index, len(left_arr)):
+        merge_arr[left_index + right_index] = left_arr[i]
 
-arr = rand.random_array([None] * 20)
+    return merge_arr
+
+arr = random_array([None] * 20)
 
 print(arr)
 
-arr_out = mergeSort(arr)
+arr_out = merge_sort(arr)
 
 print(arr_out)
-
-
