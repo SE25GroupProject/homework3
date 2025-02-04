c0053fb4d1a7e10e675a5dd6684edbdd73fec80b
crjone24 <crjone24@ncsu.edu>
2025-02-04 18:10:25 -0500

    Ran reports for static analysis tools and made changes accordingly.
    Updated the hw debugging files based on the reports from our static analysis tools (pylint, pyflakes, and radon)

--- src/hw2_debugging.py
+++ src/hw2_debugging.py
@@ -1,47 +1,88 @@
-import src.rand as rand
+"""Module providing two sorting functions."""
 
-def mergeSort(arr):
-    if (len(arr) == 1):
-        return arr
+import rand
 
-    half = len(arr)//2
 
-    return recombine(mergeSort(arr[:half]), mergeSort(arr[half:]))
+def merge_sort(unsorted_arr):
+    """
+    Merge sort implementation that takes in an unsorted array and outputs a sorted one.
+    """
 
-def recombine(leftArr, rightArr):
-    leftIndex = 0
-    rightIndex = 0
-    mergeArr = [None] * (len(leftArr) + len(rightArr))
-    while leftIndex < len(leftArr) and rightIndex < len(rightArr):
-        if leftArr[leftIndex] < rightArr[rightIndex]:
-            mergeArr[leftIndex + rightIndex] = leftArr[leftIndex]
-            leftIndex += 1
+    if len(unsorted_arr) == 1:
+        return unsorted_arr
+
+    half = len(unsorted_arr)//2
+
+    return recombine(merge_sort(unsorted_arr[:half]), merge_sort(unsorted_arr[half:]))
+
+
+def recombine(left_arr, right_arr):
+    """The function that recombines the array during merge sort."""
+
+    left_index = 0
+    right_index = 0
+    merge_arr = [None] * (len(left_arr) + len(right_arr))
+    while left_index < len(left_arr) and right_index < len(right_arr):
+        if left_arr[left_index] < right_arr[right_index]:
+            merge_arr[left_index + right_index] = left_arr[left_index]
+            left_index += 1
         else:
-            mergeArr[leftIndex + rightIndex] = rightArr[rightIndex]
-            rightIndex += 1
+            merge_arr[left_index + right_index] = right_arr[right_index]
+            right_index += 1
 
-    while(rightIndex < len(rightArr)):
-        mergeArr[leftIndex + rightIndex] = rightArr[rightIndex]
-        rightIndex += 1
+    while right_index < len(right_arr):
+        merge_arr[left_index + right_index] = right_arr[right_index]
+        right_index += 1
 
-    while(leftIndex < len(leftArr)):
-        mergeArr[leftIndex + rightIndex] = leftArr[leftIndex]
-        leftIndex += 1
+    while left_index < len(left_arr):
+        merge_arr[left_index + right_index] = left_arr[left_index]
+        left_index += 1
 
-    for i in range(rightIndex, len(rightArr)):
-        mergeArr[leftIndex + rightIndex] = rightArr[i]
-    
-    for i in range(leftIndex, len(leftArr)):
-        mergeArr[leftIndex + rightIndex] = leftArr[i]
+    for i in range(right_index, len(right_arr)):
+        merge_arr[left_index + right_index] = right_arr[i]
 
-    return mergeArr
+    for i in range(left_index, len(left_arr)):
+        merge_arr[left_index + right_index] = left_arr[i]
+
+    return merge_arr
 
 arr = rand.random_array([None] * 20)
 
+print("Unsorted Array:")
 print(arr)
 
-arr_out = mergeSort(arr)
+arr_out = merge_sort(arr)
 
+print("Array Using Merge Sort")
 print(arr_out)
+print()
 
 
+def selection_sort(unsorted_arr):
+    """An implementation of Selection sort that takes an unsorted
+    array and returns a sorted one."""
+
+    sorted_arr = unsorted_arr
+
+    for num in enumerate(sorted_arr):
+        i = num[0]
+        smallest_idx = i
+
+        for j in range(i, len(sorted_arr)):
+
+            if sorted_arr[j] < sorted_arr[smallest_idx]:
+                smallest_idx = j
+
+        temp = sorted_arr[i]
+        sorted_arr[i] = sorted_arr[smallest_idx]
+        sorted_arr[smallest_idx] = temp
+
+    return sorted_arr
+
+arr = rand.random_array([None] * 20)
+
+print("Unsorted Array")
+print(arr)
+
+print("Array sorted with Selection Sort")
+print(selection_sort(arr))
--- src/rand.py
+++ src/rand.py
@@ -1,6 +1,10 @@
+"""The file that helps create an array of numbers with values from 1 to 20."""
+
 import random
 
 def random_array(arr):
-    for i in range(len(arr)):
+    """takes in an empty array and fills it with random numbers from 1 to 20"""
+    for item in enumerate(arr):
+        i = item[0]
         arr[i] = random.randint(1, 20)
     return arr
